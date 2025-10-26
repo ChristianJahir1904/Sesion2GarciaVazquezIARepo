1e60c30283cfc62dbdf0daa9c17de6917dd1db38
ChristianJahir1904 <christianmoonwalker1904@gmail.com>
2025-10-25 19:01:02 -0600

    Version 1.2 Garcia V Sesion 2 IA JAVA

---
+++ src/estructuradedatos/ArrayList1.java
@@ -0,0 +1,23 @@
+
+package estructuradedatos;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+    public static void main(String[] args) {
+        ArrayList<String> listaNombres=new ArrayList<>();
+        
+        listaNombres.add("Christian");
+        listaNombres.add("Bryan");
+        listaNombres.add("Adrian");
+        listaNombres.add("Nestor");
+        listaNombres.add("Felix");
+        listaNombres.add("Gabriel");
+        listaNombres.add("Jose");
+        listaNombres.add("Tere");
+        
+        for (String listaNombre : listaNombres) {
+            System.out.println("Nombre: "+listaNombre+"\nSiiuuu");
+        }
+    }
+}
